b308d4a4ec4be2189cdd21952625274a853c43d6
Ayas V <ayasmohamed0910@gmail.com>
2026-08-26 00:42:26 +0530

    Now I complete the old way of handle the request and response for the llm

--- telusko-app1.py
+++ telusko-app1.py
@@ -1,14 +1,35 @@
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
 
-url = "https://api.openai.com/v1/completions"
+url = "https://api.openai.com/v1/chat/completions"
 
 headers = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {api_key}"
 }
 
+payload = {
+    "model": "gpt-4o-mini",
+    "messages": [
+        {
+            "role": "user",
+            "content": "what is AI in short",
+        }
+    ]
+}
+
+response = requests.post(
+    url,
+    headers = headers,
+    json = payload
+)
+
+print("-----------------------------------------")
+print(response.json()["choices"][0]["message"]["content"])
+print("-----------------------------------------")
+print(response.json())
