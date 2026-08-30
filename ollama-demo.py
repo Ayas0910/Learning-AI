e82029d051e386dde79a95d8e9297ffada5e371e
Ayas V <ayasmohamed0910@gmail.com>
2026-08-30 23:24:51 +0530

    how to send image through the api

--- ollama-demo.py
+++ ollama-demo.py
@@ -1,28 +1,7 @@
-from dotenv import load_dotenv
-from langchain.messages import HumanMessage
-from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
-load_dotenv()
+model = ChatOllama(model="mistral:latest",temperature = 0)
 
-model = ChatOpenAI(
-    model = "gpt-4o-mini"
-)
-
-image_url = "https://tse1.explicit.bing.net/th/id/OIP.3BW94N7eK3qeOX7-EUVGXgHaI4?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
-
-url_message = HumanMessage(
-    content=[
-        {
-            "type":"text",
-            "text": "Describe the message in two sentence"
-        },
-        {
-            "type":"image",
-            "url": "image_url"
-        }
-        
-    ]
-)
-response = model.invoke([url_message])
+response = model.invoke("What is your name?")
 
 print(response.content)
