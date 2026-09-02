eeb665f26a10af65201ac16f56fd2c63215e4984
Ayas V <ayasmohamed0910@gmail.com>
2026-09-02 17:38:06 +0530

    I do the stream for local model

--- ollama-demo.py
+++ ollama-demo.py
@@ -1,7 +1,26 @@
 from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 model = ChatOllama(model="mistral:latest",temperature = 0)
 
 response = model.invoke("What is your name?")
 
 print(response.content)
+
+chain = (ChatPromptTemplate.from_messages([
+    (
+        "system",
+        "You are a concise assistant"
+    ),
+    (
+        "user",
+        "{questions}"
+    )
+]) | model | StrOutputParser())
+
+for piece in chain.stream({
+    "questions": "Why most companies use local ai model"
+}):
+    print(piece,end="",flush=True)
+
